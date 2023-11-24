f2b3bb6131134e7d2c5861f6619034cf683d93de
Vasjan Çupri <vasjancupri04@gmail.com>
2023-11-24 21:08:14 +0100

    Optimized

--- src/utils/useCardsFetcher.tsx
+++ src/utils/useCardsFetcher.tsx
@@ -3,32 +3,36 @@ import { ICard } from '../App'
 
 const useCardsFetcher = (size: number) => {
   const [arrayToReturn, setArrayToReturn] = createSignal<ICard[]>([])
-  const [error, setError] = createSignal<Error | null>(null)
+  const [error, setError] = createSignal<any>(null)
   const [loading, setLoading] = createSignal<boolean>(true)
 
   createEffect(() => {
+    const fetchPokemon = async (id: Number) => {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      const data = await response.json()
+      return {
+        name: data.species.name,
+        cardImage: !data.sprites.front_default
+          ? data.sprites.other['official-artwork'].front_default
+          : data.sprites.front_default,
+        clicked: false,
+      }
+    }
     const fetchData = async () => {
-      const tempArr: ICard[] = []
+      const promisesArray: ICard[] = []
       let randomi = Math.floor(Math.random() * 752) + 1
       for (let i = randomi; i < randomi + size; i++) {
-        await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
-          .then((result) => result.json())
-          .then((data) => {
-            tempArr.push({
-              name: data.species.name,
-              cardImage: !data.sprites.front_default
-                ? data.sprites.other['official-artwork'].front_default
-                : data.sprites.front_default,
-              clicked: false,
-            })
-          })
-          .catch((error) => {
-            setError(error)
-            setLoading(false)
-          })
+        promisesArray.push(await fetchPokemon(i))
       }
-      setArrayToReturn(tempArr)
-      setLoading(false)
+
+      try {
+        const data = await Promise.all(promisesArray)
+        setArrayToReturn(data)
+        setLoading(false)
+      } catch (err) {
+        setError(err)
+        setLoading(false)
+      }
     }
 
     fetchData()
