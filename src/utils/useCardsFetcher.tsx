84b71485080a5c140550799ecf9edc1352ba81a6
Vasjan Çupri <vasjancupri04@gmail.com>
2023-11-24 21:03:20 +0100

    Optimized the code

--- src/utils/useCardsFetcher.tsx
+++ src/utils/useCardsFetcher.tsx
@@ -1,34 +1,39 @@
-import { useEffect, useState } from "react"
-import { ICard } from "../App"
+import { useEffect, useState } from 'react'
+import { ICard } from '../App'
 
 const useCardsFetcher = (size: number) => {
   const [arrayToReturn, setArrayToReturn] = useState<ICard[]>([])
-  const [error, setError] = useState<Error | null>(null)
+  const [error, setError] = useState<any>(null)
   const [loading, setLoading] = useState<boolean>(true)
 
   useEffect(() => {
+    const fetchPokemon = async (id: Number) => {
+      const result = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      const data = await result.json()
+
+      return {
+        name: data.species.name,
+        cardImage: !data.sprites.front_default
+          ? data.sprites.other['official-artwork'].front_default
+          : data.sprites.front_default,
+        clicked: false,
+      }
+    }
     const fetchData = async () => {
-      const tempArr: ICard[] = []
+      const fetchPromises: ICard[] = []
       let randomi = Math.floor(Math.random() * 752) + 1
       for (let i = randomi; i < randomi + size; i++) {
-        await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
-          .then((result) => result.json())
-          .then((data) => {
-            tempArr.push({
-              name: data.species.name,
-              cardImage: !data.sprites.front_default
-                ? data.sprites.other["official-artwork"].front_default
-                : data.sprites.front_default,
-              clicked: false,
-            })
-          })
-          .catch((error) => {
-            setError(error)
-            setLoading(false)
-          })
+        fetchPromises.push(await fetchPokemon(i))
       }
-      setArrayToReturn(tempArr)
-      setLoading(false)
+
+      try {
+        const data = await Promise.all(fetchPromises)
+        setArrayToReturn(data)
+        setLoading(false)
+      } catch (err) {
+        setError(err)
+        setLoading(false)
+      }
     }
 
     fetchData()
